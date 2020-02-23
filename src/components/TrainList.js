a4268c1192d2f28842b1e1b24ab5b023129f8f29
kieranphilipbrown <kieranpbrown@gmail.com>
2020-02-23 18:15:46 +0000

    Added responsive styling with styled components.

--- src/components/TrainList.js
+++ src/components/TrainList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 class TrainList extends React.Component {
 
@@ -44,13 +45,14 @@ class TrainList extends React.Component {
 						</ul>
 					</div>
 
-					<div style={{ margin: "68px auto 0", maxWidth: "1200px", padding: "20px 0", display: 'flex', justifyContent: 'space-between', listStyleType: 'none', paddingLeft: "0"}}>
-						<div style={cardTwo}><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/><path d="M0 0h24v24H0z" fill="none"/></svg> <p>Date: {date}</p></div>
-						<div style={cardTwo}><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>  <p>Time: {timeOfDay}</p></div>
-						<div style={cardTwo}><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>  <p>Station Name: {stationName}</p></div>
-					</div>
+					<div style={pageWidth}>
+					<InfoContainer>
+						<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/><path d="M0 0h24v24H0z" fill="none"/></svg> <p>Date: {date}</p></CardTwo>
+						<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>  <p>Time: {timeOfDay}</p></CardTwo>
+						<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>  <p>Station Name: {stationName}</p></CardTwo>
+					</InfoContainer>
 
-					<ul style={grid}>
+					<Grid>
 						{trainList && trainList.length > 0 &&
 							trainList.map((train, i) =>
 								<li style={card} key={i}>
@@ -60,7 +62,7 @@ class TrainList extends React.Component {
 									{
 										train.status === "LATE"
 										?
-										<span style={{fontSize: "14px", background: "red", border: "1px solid red", padding: "5px 10px", display: "inline-block", borderRadius: "3px"}}>
+										<span style={{fontSize: "14px", background: "#ff5656", border: "1px solid #e62525", padding: "5px 10px", display: "inline-block", borderRadius: "3px"}}>
 											{train.status}: Expected {train.expected_arrival_time}
 										</span>
 										:
@@ -74,12 +76,23 @@ class TrainList extends React.Component {
 								</li>
 							)
 						}
-					</ul>
+					</Grid>
+					</div>
 
 				</main>
 			</>
 		);
 	}
+};
+
+const size = {
+	tablet: '768px',
+	desktop: '960px'
+}
+
+const device = {
+	tablet: `(min-width: ${size.tablet})`,
+	desktop: `(min-width: ${size.desktop})`
 };
 
 const pageWidth = {
@@ -96,15 +109,34 @@ const trainButton = {
 	padding: "10px 15px",
 }
 
-const grid = {
-	display: 'grid',
-	gridTemplateColumns: '1fr 1fr 1fr',
-	gridGap: '40px',
-	listStyleType: 'none',
-	margin: "0 auto 40px",
-	maxWidth: "1200px",
-	paddingLeft: "0"
-}
+const InfoContainer = styled.div`
+	display: block;
+	list-style-type: none;
+	margin: 0 auto;
+	maxWidth: 1200px;
+	padding: 20px 0;
+
+	@media ${device.tablet} {
+		display: flex;
+		justify-content: space-between;
+	}
+`;
+
+const Grid = styled.ul`
+	display: grid;
+	flex-direction: column;
+	grid-template-columns: 1fr;
+	grid-gap: 40px;
+	list-style-type: none;
+	margin: 0 auto 40px;
+	max-width: 1200px;
+	padding-left: 0;
+
+	@media ${device.tablet} {
+		flex-direction: row;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+`;
 
 const card = {
 	background: '#fff',
@@ -113,25 +145,26 @@ const card = {
 	padding: '1.5rem'
 }
 
-const cardTwo = {
-	alignItems: "center",
-	background: '#fff',
-	borderRadius: '18px',
-	display: "flex",
-	boxShadow: "0 3px 13px -2px rgba(0,0,0,.15)",
-	padding: '1.5rem'
-}
+const CardTwo = styled.div`
+	align-items: center;
+	background: #fff;
+	border-radius: 18px;
+	display: flex;
+	box-shadow: 0 3px 13px -2px rgba(0,0,0,.15);
+	margin-bottom: 15px;
+	padding: 0.5rem;
+
+	@media ${device.tablet} {
+		margin-bottom: 0;
+		padding: 1.5rem;
+	}
+`
 
 const buttonContainer = {
 	background: "white",
 	borderBottom: "1px solid #cecece",
 	display: "flex",
 	justifyContent: "center",
-	left: "0",
-	position: "fixed",
-	right: "0",
-	top: "0",
-	width: "100%",
 }
 
 export default TrainList;
