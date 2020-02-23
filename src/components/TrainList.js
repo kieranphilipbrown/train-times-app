448b8cbaed5f3d76502e504ed46a8df179c2cfcb
kieranphilipbrown <kieranpbrown@gmail.com>
2020-02-23 22:13:38 +0000

    added search inputs to input any station name.

--- src/components/TrainList.js
+++ src/components/TrainList.js
@@ -8,13 +8,48 @@ class TrainList extends React.Component {
 		date: '',
 		timeOfDay: '',
 		stationName: '',
-		trainList: []
+		trainList: [],
+		stationFrom: '',
+		stationTo: ''
 	}
 
 	trainListCallback = (e) => {
 		const trainButton = e.target.id;
 		const destination = e.target.dataset.des;
 		fetch(`https://transportapi.com/v3/uk/train/station/${trainButton}/live.json?app_id=ba2a7c92&app_key=761d4fbe8af3114e3dc16dedf2b91443&calling_at=${destination}&darwin=true&train_status=passenger`)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				this.setState({
+					date: data.date,
+					timeOfDay: data.time_of_day,
+					stationName: data.station_name,
+					trainList: data.departures.all
+				})
+			});
+	}
+
+	handleFromChange = (event) => {
+		this.setState({
+			stationFrom: event.target.value,
+		});
+	}
+
+	handleToChange = (event) => {
+		this.setState({
+			stationTo: event.target.value,
+		});
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		//console.log(`${this.state.stationFrom} ${this.state.stationTo}`);
+
+		const stationFrom = this.state.stationFrom;
+		const stationTo = this.state.stationTo;
+
+		fetch(`https://transportapi.com/v3/uk/train/station/${stationFrom}/live.json?app_id=ba2a7c92&app_key=761d4fbe8af3114e3dc16dedf2b91443&calling_at=${stationTo}&darwin=true&train_status=passenger`)
 			.then((response) => {
 				return response.json();
 			})
@@ -45,18 +80,29 @@ class TrainList extends React.Component {
 						</ul>
 					</div>
 
+					<SearchContainer>
+						<form onSubmit={this.handleSubmit}>
+							<label htmlFor="stationfrom">Where from?</label>
+							<input id="stationfrom" type="text" placeholder="Station Name" value={this.state.stationFrom} onChange={this.handleFromChange} />
+
+							<label htmlFor="stationto">Where to?</label>
+							<input id="stationto" type="text" placeholder="Station Name" value={this.state.stationTo} onChange={this.handleToChange} />
+							<button>Search</button>
+						</form>
+					</SearchContainer>
+
 					<div style={pageWidth}>
 						<InfoContainer>
 							<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/><path d="M0 0h24v24H0z" fill="none"/></svg> <p>Date: {date}</p></CardTwo>
 							<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>  <p>Time: {timeOfDay}</p></CardTwo>
-							<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>  <p>Station Name: {stationName}</p></CardTwo>
+							<CardTwo><svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg><p>Station: {stationName}</p></CardTwo>
 						</InfoContainer>
 
 						<Grid>
 							{trainList && trainList.length > 0 &&
 								trainList.map((train, i) =>
 									<li style={card} key={i}>
-										{console.log(train)}
+										{/* {console.log(train)} */}
 										<p>Train to: <span style={{fontWeight: "bold"}}>{train.destination_name}</span></p>
 										<p style={{fontSize: "14px", display: "block", marginBottom: "15px"}}>From: {train.origin_name}</p>
 										{
@@ -109,11 +155,17 @@ const trainButton = {
 	padding: "10px 15px",
 }
 
+const SearchContainer = styled.div`
+	background: white;
+	border-bottom: 1px solid #cecece;
+	padding: 15px 0;
+`;
+
 const InfoContainer = styled.div`
 	display: block;
 	list-style-type: none;
 	margin: 0 auto;
-	maxWidth: 1200px;
+	max-width: 1200px;
 	padding: 20px 0;
 
 	@media ${device.tablet} {
@@ -152,7 +204,16 @@ const CardTwo = styled.div`
 	display: flex;
 	box-shadow: 0 3px 13px -2px rgba(0,0,0,.15);
 	margin-bottom: 15px;
+	min-width: 220px;
 	padding: 0.5rem;
+
+	&:nth-child(2) {
+		margin: 0 15px;
+	}
+
+	&:nth-child(3) {
+		flex: 1;
+	}
 
 	@media ${device.tablet} {
 		margin-bottom: 0;
