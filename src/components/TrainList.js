6a5deed555c5c8f21d106870457c9fa8cc1d3b35
Kieran Brown <Kieran.Brown@peninsula-uk.com>
2020-02-24 10:52:07 +0000

    Added icon to quick train buttons.

--- src/components/TrainList.js
+++ src/components/TrainList.js
@@ -82,10 +82,10 @@ class TrainList extends React.Component {
 			<>
 				<main className="main-content">
 					<div style={buttonContainer}>
-						<TrainButton id="NLW" data-des="MCV" onClick={this.trainListCallback}>Nlw => Mcv</TrainButton>
-						<TrainButton id="MCV" data-des="NLW" onClick={this.trainListCallback}>Mcv => Nlw</TrainButton>
-						<TrainButton id="NLW" data-des="MAN" onClick={this.trainListCallback}>Nlw => Man</TrainButton>
-						<TrainButton id="MAN" data-des="NLW" onClick={this.trainListCallback}>Man => Nlw</TrainButton>
+						<TrainButton id="NLW" data-des="MCV" onClick={this.trainListCallback}><svg style={{width: "20px", verticalAlign: "middle", marginTop: "-2px", marginRight: "8px", marginTop: "-3px", fill: "#ffffff"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>Nlw => Mcv</TrainButton>
+						<TrainButton id="MCV" data-des="NLW" onClick={this.trainListCallback}><svg style={{width: "20px", verticalAlign: "middle", marginTop: "-2px", marginRight: "8px", marginTop: "-3px", fill: "#ffffff"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>Mcv => Nlw</TrainButton>
+						<TrainButton id="NLW" data-des="MAN" onClick={this.trainListCallback}><svg style={{width: "20px", verticalAlign: "middle", marginTop: "-2px", marginRight: "8px", marginTop: "-3px", fill: "#ffffff"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>Nlw => Man</TrainButton>
+						<TrainButton id="MAN" data-des="NLW" onClick={this.trainListCallback}><svg style={{width: "20px", verticalAlign: "middle", marginTop: "-2px", marginRight: "8px", marginTop: "-3px", fill: "#ffffff"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>Man => Nlw</TrainButton>
 					</div>
 
 					<SearchContainer>
@@ -187,7 +187,7 @@ const TrainButton = styled.button`
 	font-family: Comfortaa;
 	font-size: 12px;
 	margin: 0 10px;
-	min-width: 100px;
+	min-width: 130px;
 	outline: none;
 	padding: 10px 15px;
 	transition: 0.4s ease;
