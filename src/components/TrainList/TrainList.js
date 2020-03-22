45084167ce39ccc7bb49f5191eaf7fff39e61322
kieranphilipbrown <kieranpbrown@gmail.com>
2020-03-22 11:36:49 +0000

    Remove test logo from train list.

--- src/components/TrainList/TrainList.js
+++ src/components/TrainList/TrainList.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Modal from '../Modal/Modal';
 
-import { ReactComponent as Logo } from '../../assets/images/tp-express-icon.svg';
-
 const buttonData = [
 	{
 		id: 'NLW',
@@ -136,10 +134,9 @@ class TrainList extends React.Component {
 					</Loader>
 				}
 				<main className="main-content">
-					<Logo />
 					<div style={buttonContainer}>
 						{
-							buttonData.map((button, i) => 
+							buttonData.map((button, i) =>
 								<TrainButton key={`${button.id}-${i}`} id={button.id} data-des={button.destination} onClick={this.trainListCallback}><svg style={{width: "20px", verticalAlign: "middle", marginRight: "8px", marginTop: "-3px", fill: "#ffffff"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>{button.label}</TrainButton>
 							)
 						}
