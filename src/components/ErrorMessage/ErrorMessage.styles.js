8fe1dad6927708e7c95da7f9ac35367f576952b1
kieranphilipbrown <kieranpbrown@gmail.com>
2020-07-02 21:33:41 +0100

    Update bg colour of error message.

--- src/components/ErrorMessage/ErrorMessage.styles.js
+++ src/components/ErrorMessage/ErrorMessage.styles.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const StyledErrorMessage = styled.div `
     align-items: center;
-    background: #ffffff;
+    background-color: rgba(250,92,124,.18);
     border-bottom: 1px solid #e9e9e9;
     box-shadow: 0 3px 13px -2px rgba(0,0,0,.15);
+    color: #fa5c7c;
     display: flex;
     justify-content: center;
     padding: 0.7rem;
