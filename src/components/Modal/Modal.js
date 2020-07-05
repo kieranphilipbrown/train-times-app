11c35dc6654c7a1e63ff41409c97427c3f85bbf1
kieranphilipbrown <kieranpbrown@gmail.com>
2020-07-05 07:09:17 +0100

    Add fallback for null data.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -50,13 +50,27 @@ class Modal extends React.Component {
                         <div>
                             <p style={{alignItems: 'center', display: 'flex', fontSize: '14px'}}>
                                 <svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>
-                                Arrives: {aimed_arrival_time}
+                                Arrives: 
+                                {
+                                    expected_arrival_time === null || expected_arrival_time === ''
+                                    ?
+                                    `N/A`
+                                    :
+                                    aimed_arrival_time
+                                }
                             </p>
                         </div>
                         <div>
                             <p style={{alignItems: 'center', display: 'flex', fontSize: '14px'}}>
                                 <svg style={{marginRight: "8px", marginTop: "-3px", fill: "#8e8e8e"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>
-                                Departs: {aimed_departure_time}
+                                Departs: 
+                                {
+                                    aimed_departure_time === null || aimed_departure_time === ''
+                                    ?
+                                    `N/A`
+                                    :
+                                    aimed_departure_time
+                                }
                             </p>
                         </div>
                     </ModalMiddle>
